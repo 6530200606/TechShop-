7e8c9b0ad0029e8aa0bd9a5a7ee81464634ca6b2
Chaiwat Setho <156393111+6530200606@users.noreply.github.com>
2025-02-24 02:02:30 +0700

    Delete Backend directory

--- Backend/routes/brandRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as brandController from "../controllers/brandController.js"
-
-const router = express.Router()
-
-router.get('/brands', brandController.getAllbrands)
-
-export default router
--- Backend/routes/typeRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as typeController from "../controllers/typeController.js"
-
-const router = express.Router()
-
-router.get('/types', typeController.getAllType)
-
-export default router
